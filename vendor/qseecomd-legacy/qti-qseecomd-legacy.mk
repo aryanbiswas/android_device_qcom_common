a2465eb4c2ac7e2d47176d40256966547238812c
CuriousNom <aryanbiswas7585@gmail.com>
2026-03-14 13:07:08 +0530

    common: Start building shared libs again
    Revert "common: Stop explicitly building shared libs"
    
    This reverts commit f05be49f9460eaf47d91a10a5e6e8a6e357989d7.
    
    Change-Id: I21b6491c1f62811e2dbaf95a9b96bded6cd92867

--- vendor/qseecomd-legacy/qti-qseecomd-legacy.mk
+++ vendor/qseecomd-legacy/qti-qseecomd-legacy.mk
@@ -3,5 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_PACKAGES += \
+    libdrm.vendor
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/qseecomd-legacy/qseecomd-legacy-vendor.mk)
