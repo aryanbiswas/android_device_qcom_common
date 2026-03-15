8096cab96d31751b17633be4cdfdb26f1e4d6e8d
CuriousNom <aryanbiswas7585@gmail.com>
2026-03-15 11:55:37 +0530

    common: Don't error out of TARGET_BOARD_PLATFORM
    * Your device variables should be defined in your BoardConfig now.
    
    Change-Id: I5ecf9784c5b6b8980d472cd4c87335306367f167

--- common.mk
+++ common.mk
@@ -5,10 +5,6 @@
 
 QCOM_COMMON_PATH := device/qcom/common
 
-ifeq ($(TARGET_BOARD_PLATFORM),)
-$(error "TARGET_BOARD_PLATFORM is not defined yet, please define in your device makefile so it's accessible to QCOM common.")
-endif
-
 # List of QCOM targets.
 MSMSTEPPE := sm6150
 TRINKET := trinket
